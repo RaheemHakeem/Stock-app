2658b196fdb857c0eedb4fa2011dc8ea08cac967
Rav <raheemhakeem25@gmail.com>
2023-11-06 15:06:22 +0100

    Before adding context

---
+++ src/contexts/WatchListContext.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+    return(
+        <AppContext.Provider>
+            {children}
+        </AppContext.Provider>
+    )
+};
+
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+export { AppProvider, AppContext };
